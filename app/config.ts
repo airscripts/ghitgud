cb1c02a2fff5416dbd4d8bc78b3d1dce4f6f1cd7
Francesco Sardone <francesco@airscript.it>
2025-06-12 15:16:02 +0000

    tests: temporary fix to config mocking

--- app/config.ts
+++ app/config.ts
@@ -2,8 +2,8 @@ import functions from "./functions";
 import "dotenv/config";
 
 const config = {
-  repo: process.env.GHITGUD_GITHUB_REPO || functions.config.read("repo"),
-  token: process.env.GHITGUD_GITHUB_TOKEN || functions.config.read("token"),
+  repo: process.env.GHITGUD_GITHUB_REPO || functions?.config.read("repo"),
+  token: process.env.GHITGUD_GITHUB_TOKEN || functions?.config.read("token"),
 };
 
 export default config;
